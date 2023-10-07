a2875f064e0dcb78028d07e7b0927e8bb63a87a2
dharanesh-2002 <tkdharanesh@gmail.com>
2023-10-07 11:34:20 +0530

    Basic

---
+++ Classes/Basic.php
@@ -0,0 +1,67 @@
+<?php
+readonly class Circle
+{
+    public int $radius;
+    public function __construct($radius)
+    {
+        $this->radius = $radius;
+    }
+    public function area()
+    {
+        return pi() * $this->radius * $this->radius;
+    }
+    public function circumferance()
+    {
+        return 2 * pi() * $this->radius;
+    }
+}
+
+//Extend Class
+class Display
+{
+    public $Message;
+    public function displ($Message)
+    {
+        return $this->Message = $Message;
+    }
+}
+class GetMessage extends Display
+{
+    public $greet;
+    public function displ1($greet)
+    {
+        $this->greet = $greet;
+        parent::displ($greet);
+    }
+}
+class Vehicle
+{
+    public $vehicleType;
+    public $model;
+    public function display($vehicleType, $model)
+    {
+        echo "Type :". $this->vehicleType = $vehicleType ."<br> Model :".  $this->model = $model. "<br>";
+    }
+}
+class Van extends Vehicle
+{
+    public $type;
+    public $year;
+    public function get($type, $year)
+    {
+        $this->type = $type;
+        $this->year = $year;
+        parent::display($type, $year);
+    }
+}
+class Truck extends Vehicle
+{
+    protected $type;
+    protected $year;
+    public function get($type, $year)
+    {
+        $this->type = $type;
+        $this->year = $year;
+        parent::display($type, $year);
+    }
+}
--- Classes/index.php
+++ Classes/index.php
@@ -1,16 +1,32 @@
 <?php
 require_once 'Car.php';
+include_once 'Basic.php';
 
 $car1 = new Car("BMW", "2023", "Red");
 $car2 = new Car("Ford", "2019");
 $car1->carInfo();
 $car2->carInfo();
 $car1->setBrand("Honda");
-echo "Car1_Brand: ". $car1->getBrand();
+//echo "Car1_Brand: ". $car1->getBrand();
 $car2->setModel(2023);
-echo "<br>Car2_Model: ". $car2->getModel();
-echo "<br><br>";
-echo $car1->carInfo();
-echo $car2->carInfo();
+//echo "<br>Car2_Model: ". $car2->getModel();
+//echo "<br><br>";
+//echo $car1->carInfo();
+//echo $car2->carInfo();
+
+$math= new Circle(5);
+//echo $math->area();
+//$math->radius=10; => Throws an error
+
+$obj=new GetMessage();
+$obj->displ1("Hello Everyone!!!");
+// echo $obj::class;
+
+$vehicle= new Van();
+//$vehicle->get("Tata 407",2009);
+$vehicle= new Truck();
+//$vehicle->get("Ashok Leyland",2009);
+
+
 
 ?>
