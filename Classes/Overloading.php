ab3d3819a4c6a22a2ecdcdb4012da4d7d32177e3
dharanesh-2002 <tkdharanesh@gmail.com>
2023-11-20 10:12:17 +0530

    php

--- Classes/Overloading.php
+++ Classes/Overloading.php
@@ -29,9 +29,7 @@ class Overloading1{
     public $method;
     public $args=[];
     public function __call($method, $args){
-        $this->method = $method;
-        $this->args=$args;
-        echo "$this->method having arguments like $this->args HI <br>";
+        echo "$this->method having arguments like". implode(", ", $args) ." HI <br>";
     }
     public static function __callStatic( $method, $args){
         echo $method . " having an arguments like ". implode(", ", $args) ."<br>";
@@ -41,4 +39,25 @@ $obj1=new Overloading1();
 $obj1->someUndefinedMethod (1,2,3,4,5);
 Overloading1::someUndefinedStaticMethod(1,2,3);
 
+
+//Object Iteration
+class Iteration{
+    public $value1=100;
+    public $value2=1000;
+    public $value3=10;
+
+    protected $value4="Protected";
+    private $value5="Private";
+    public function iterative(){
+        foreach ($this as $key=>$value){
+            echo $key .">>". $value ."<br>";
+        }
+    }
+}
+
+$IT=new Iteration();
+$IT->iterative();
+foreach($IT as $key=>$value){
+    echo $key. "> ". $value ."<br>";
+}
 
